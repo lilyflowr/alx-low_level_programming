56f8f21e04806b75318670398c7128266a83f59e
Ibinabo Adiela <99748338+lilyflowr@users.noreply.github.com>
2022-10-17 05:57:50 +0100

    Update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*
  *
- * main-entry point for the program
+ * main - entry point for the program
  *
- * Return:0 if no errors, return non zero if errors
+ *Return:0 if no errors, return non zero if errors
  */
 int main(void)
 {
