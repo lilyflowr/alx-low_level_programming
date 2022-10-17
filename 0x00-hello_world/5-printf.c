111cf8a1a6bd788fcf88ae264a5283da1c56bd69
Ibinabo Adiela <99748338+lilyflowr@users.noreply.github.com>
2022-10-17 06:56:55 +0100

    Let's see

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-/*
- * main - entry point for the program.
- * 
- * Description: 'enabling easy programing'
- * 
+#include <stdio.h>
+
+/**
+ * main-entry point for the program.
+ *
  * Return:0 if no errors, return non zero if errors
- */
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
