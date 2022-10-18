810610bf0c9853354f32d66ce956dc5a19cf403d
Ibinabo Adiela <99748338+lilyflowr@users.noreply.github.com>
2022-10-18 01:21:25 +0100

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  *main - entry point for the program
  *
- *return: 0 if no errors,return non
+ *return : 0 if no errors,return non
 */
 
 int main() {
-    printf("with proper grammar, but the outcome is a piece of art,");
+    printf("with proper grammar,but the outcome is a piece of art,");
 
     return 0;
 }
