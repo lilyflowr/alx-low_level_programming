3ffac4bba0ee929b26e3480700e5f8d5467f8cbf
lilyflowr <ibadiela@gmail.com>
2022-10-27 17:52:45 +0100

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
+ * _strncat - concatenates two strings.
  *
  * @src: The source of strings
  * @dest: The destination of the string
