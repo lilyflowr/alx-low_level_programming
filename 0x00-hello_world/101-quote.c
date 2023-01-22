be98ae4b1a16a222582ac5d0c74ea0dd7c9ef703
Ibinabo Adiela <99748338+lilyflowr@users.noreply.github.com>
2023-01-22 06:00:59 +0000

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,13 @@
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban
- */
-
-#include <unistd.h>
-
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - Entry point
  *
- * Return: Always 1.
+ * Return: Always 0 (Success)
  */
+#include <stdio.h>
+#include <unistd.h>
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
+
